b954402222c2f09241f0f7d0c8ef4530c1ba556f
Super User <root@SOMAX-SJM.localdomain>
2025-12-10 16:37:15 +0900

    슬랙 추가

--- back/crud/__init__.py
+++ back/crud/__init__.py
@@ -1,4 +1,5 @@
-from .user import get_user_by_username, create_user
+from .user import get_user_by_username, create_user, count_users # [New] count_users 추가
 from .game_data import get_user_progress, update_user_progress, get_user_modules, update_user_modules
-from .report import create_battle_record, get_recent_reports, get_history_reports, get_full_report, get_cutoff_date
+# [New] 아래 줄에 count_reports 추가
+from .report import create_battle_record, get_recent_reports, get_history_reports, get_full_report, get_cutoff_date, count_reports 
 from .stats import get_weekly_stats, get_weekly_trends
