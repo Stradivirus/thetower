3dd8d10ed8a2f2fe8f2ac0832cc1faac5504bc9e
Super User <root@EE.localdomain>
2025-12-28 19:15:36 +0900

    프록시 설정 추가 이제 apiconfig 파일 건드릴 필요 없음

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // 8080에서 8000으로 수정
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''), // 필요시 주석 해제
+      },
+    },
+  },
 })
